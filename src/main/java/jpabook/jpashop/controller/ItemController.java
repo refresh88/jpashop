8c8be8039de69395f2fcc0c280f55d09f011500e
refresh88 <ouok88@gmail.com>
2023-07-21 12:06:36 +0900

    section7 member, item

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Item.Book;
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        // 이거 빈껍데기지만 넘겨주면 html에서 model에 들어있는 값들 추척 가능함.
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        // 지금은 그냥하지만 원래는 setter 전부 제거하고 createBook
+        // 이런식으로 파라마티로 넘기는게 더 좋은 설계임.
+        // 여기는 예제니까 걍 함 ㅋ.
+        // 실무에선 setter 다 날림
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+
+        return "items/itemList";
+    }
+}
